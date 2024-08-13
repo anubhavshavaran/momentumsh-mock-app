6faae5b843e949f54bff4ac8d299a8a015570646
Anubhav Shavaran <anubhavshavaran5678@gmail.com>
2024-08-13 23:22:36 +0530

    # Initial commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Sidebar() {
+    return (
+        <div className='flex flex-col bg-gray h-full '>
+            
+        </div>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray': '#363636',
+        'light-gray': '#595858',
+        'gray-b7': '#B7B7B7',
+        'gray-64': '#646464',
+        'gray-d9': '#D9D9D9',
+        'blue': '#009FF9',
+        'light-orange': '#FFAD62',
+        'orange': '#F27400'
+      }
+    },
+  },
+  plugins: [],
+}
+
